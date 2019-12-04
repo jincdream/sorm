1c9b9324ccb9a8b9c7a15fe0861ed7fcdd7834fe
Jin_C <jinc.cjc@alibaba-inc.com>
2019-12-04 19:31:43 +0800

    Revert "Alipay bug demo"

--- .tea/entryFiles-development/index$.web.js
+++
@@ -1,7 +0,0 @@
-require('@alipay/appx-compiler/lib/sjsEnvInit');
-require('./config$');
-
-require('../../demo/dist/field/field?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
-require('../../demo/dist/sorm/sorm?hash=c4b0edf8c04b652d90eb3e0e4bcb8eb1dcbcd9f8');
-require('../../demo/fields/fields?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
-require('../../demo/index/index?hash=af81ae0f40e619a2515574e6c6ac1378e304639f');
